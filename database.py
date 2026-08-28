4ce377e02c442130c45f73681a28b1c81954e419
verkhorubovroman-cloud <verkhorubov.roman@gmail.com>
2026-08-28 15:53:21 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+import json
+
+def load_db():
+    try:
+        with open("db.json", "r") as f:
+            return json.load(f) # Читаем JSON как список
+    except FileNotFoundError:
+        return []
+
+def save_db(data):
+    with open ("db.json", "w") as f:
+        json.dump(data, f)
+
+# Загружаем базу данных при старте
+db = load_db()
