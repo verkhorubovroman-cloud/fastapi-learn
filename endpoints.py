e4f74554bbe0b2017cea076f581312b9558d1610
verkhorubovroman-cloud <verkhorubov.roman@gmail.com>
2026-08-28 21:20:41 +0500

    Update endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, HTTPException, Depends, Query
-from pydantic import BaseModel
+from pydantic import BaseModel, AfterValidator
 from enum import Enum
 from fastapi.security import APIKeyHeader, HTTPBasic, HTTPBasicCredentials
 from secrets import compare_digest
 from auth import get_key, get_user, admin_only
 from database import db, save_db
 from typing import Annotated
+from valid import check_valid_id
+import random
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2"
+}
+
 
 class Item(BaseModel):
     name: str
@@ -20,8 +29,11 @@ class Model(str, Enum):
 
 # Функция для регистрации эндпойнтов
 def register_endpoints(app: FastAPI):
+    @app.get("/once_fixedquery/")
+    async def once_fixdquery(q: Annotated[str | None, Query(pattern = "^fixedquery$")]):
+        return q
     @app.get("/for_q/")
-    async def once_q(q : Annotadet[list[str], Query(min_ligth = 3)]):
+    async def once_q(q : Annotated[list[str], Query(min_ligth = 3, alias = "item_query", deprecated = True)] = ["foo", "bar"]):
         register = {"q" : q}
         return register
     @app.get("/models/{model}")
@@ -43,7 +55,7 @@ def register_endpoints(app: FastAPI):
         return {"item_id": item_id, "q":q}
 
     @app.put("/items/{item_id}")
-    async def update_item(item_id: int, item: Item, q: Annotated[str | None, Query(min_length= 3, max_length = 50, pattern = "^fixedquery$")]=None):
+    async def update_item(item_id: int, item: Item, q: Annotated[str | None, Query(title= "Query string", description="this is q", min_length= 3, max_length = 50)]=None):
         result = {"item_id" : item_id, **item.model_dump()}
         if q:
             result.update({"q":q})
@@ -57,6 +69,12 @@ def register_endpoints(app: FastAPI):
     @app.get("/admin/")
     async def admin_only_point(user: str = Depends(admin_only)):
         return {"message" : "Welcome, admin"}
+
+    @app.get("/not_openapi/")
+    async def nothing_openapi(hidden_query :  Annotated[str | None, Query(include_in_schema = False)] = None):
+        if hidden_query:
+            return {"hidden_query" : hidden_query}
+        return {"hidden_query" : "Not found"}
 
     @app.get("/users/{user_id}")
     async def read_user(user_id: str):
@@ -74,3 +92,11 @@ def register_endpoints(app: FastAPI):
     @app.get("/items/")
     async def read_item():
         return db
+    @app.get("/checking/")
+    async def checking(id : Annotated[str | None,   AfterValidator(check_valid_id)] = None ):
+        if id:
+            item = data.get(id)
+        else:
+            id, item = random.choice(list(data.items()))
+        return {"id" : id, "item" : item}
+        
