394fed0b5189cd1af7bc587f650197d857b9e5ca
Roman Verkhorubov <verkhorubov.roman@gmail.com>
2026-08-30 11:15:38 +0500

    Update endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Depends, Query
+# импортируем все нужные библиотеки
+from fastapi import FastAPI, HTTPException, Depends, Query, Path
 from pydantic import BaseModel, AfterValidator
 from enum import Enum
 from fastapi.security import APIKeyHeader, HTTPBasic, HTTPBasicCredentials
@@ -9,13 +10,14 @@ from typing import Annotated
 from valid import check_valid_id
 import random
 
+# создаём список для /checking/
 data = {
     "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
     "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
     "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2"
 }
 
-
+#создаём класс для значениий только которые могут быть обозначены 
 class Item(BaseModel):
     name: str
     price : float
@@ -24,24 +26,28 @@ class Item(BaseModel):
 
 class Model(str, Enum):
     a = "a"  # Разрешённое значение "a"
-    b = "e"  # Разрешённое значение "b"
+    b = "e"  # Разрешённое значение "e"
 
 
 # Функция для регистрации эндпойнтов
 def register_endpoints(app: FastAPI):
+    #эндпойнт для проверки правильности написания параметра в get запросе
     @app.get("/once_fixedquery/")
     async def once_fixdquery(q: Annotated[str | None, Query(pattern = "^fixedquery$")]):
         return q
+    #здесь находятся многие параметры query
     @app.get("/for_q/")
     async def once_q(q : Annotated[list[str], Query(min_ligth = 3, alias = "item_query", deprecated = True)] = ["foo", "bar"]):
         register = {"q" : q}
         return register
+    #определяет что ты написал в путь если не a или e тогда он выдаёт ошибку
     @app.get("/models/{model}")
     async def get_model(model: Model):
         # model: Model -- проверяет, что значение из Enum
         if model == Model.a:
                 return ["haaaallo a"]
         return ["hello e"]
+    
     # Эндпойнт для главной страницы
     @app.get("/")
     async def root():
@@ -54,13 +60,15 @@ def register_endpoints(app: FastAPI):
         # item_id: int -- параметр должен быть числом
         return {"item_id": item_id, "q":q}
 
+    #здесь всё в перемешку |: и он пока ничего не обновляет
     @app.put("/items/{item_id}")
-    async def update_item(item_id: int, item: Item, q: Annotated[str | None, Query(title= "Query string", description="this is q", min_length= 3, max_length = 50)]=None):
+    async def update_item(item_id: Annotated[int, Path(title = "The ID of the item to get")], item: Item, q: Annotated[str | None, Query(title= "Query string", description="this is q", min_length= 3, max_length = 50)]=None):
         result = {"item_id" : item_id, **item.model_dump()}
         if q:
             result.update({"q":q})
         return result
 
+    #этот эндпоинт возращает значение если овтаризация удалась
     @app.get("/users/me")
     async def read_user_me(user: str = Depends(get_user)):
         return {"user_id" : user}
@@ -70,16 +78,19 @@ def register_endpoints(app: FastAPI):
     async def admin_only_point(user: str = Depends(admin_only)):
         return {"message" : "Welcome, admin"}
 
+    #в этом коде ничего не отображается в swager значения можно ввести только через ссылку
     @app.get("/not_openapi/")
     async def nothing_openapi(hidden_query :  Annotated[str | None, Query(include_in_schema = False)] = None):
         if hidden_query:
             return {"hidden_query" : hidden_query}
         return {"hidden_query" : "Not found"}
 
+    #простая версия прошлой авторизации
     @app.get("/users/{user_id}")
     async def read_user(user_id: str):
         return {"user_id" : user_id}
-    
+
+    #через него добовлять данные в json
     @app.post("/items/")
     async def create_item(item: Item):
         #проверяем цену
@@ -89,11 +100,14 @@ def register_endpoints(app: FastAPI):
         save_db(db)
         return item
 
+    #простотр данных из json файла
     @app.get("/items/")
     async def read_item():
         return db
+
+    #проверяет правильно ли введено id 
     @app.get("/checking/")
-    async def checking(id : Annotated[str | None,   AfterValidator(check_valid_id)] = None ):
+    async def checking(id : Annotated[str | None,  AfterValidator(check_valid_id)] = None ):
         if id:
             item = data.get(id)
         else:
