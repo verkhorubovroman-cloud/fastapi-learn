c2b9cd47309b6dedb1c1c68c7a7a543b1adfe0f9
Roman Verkhorubov <verkhorubov.roman@gmail.com>
2026-08-30 11:16:23 +0500

    Update valid.py

--- valid.py
+++ valid.py
@@ -1,4 +1,5 @@
+#код для проверки id если начинается не с  isbn- или imdb-
 def check_valid_id(id : str):
-    if not id.startswith(("isbn-", "imdb")):
+    if not id.startswith(("isbn-", "imdb-")):
         raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
     return id
